fce71e4aebcc31ed1df778969ea331eaaa213a31
kendalldc <kdendtler@gmail.com>
2020-09-15 21:51:24 -0400

    css edits

--- heir/src/Components/Cart.js
+++ heir/src/Components/Cart.js
@@ -1,4 +1,26 @@
 import React from 'react';
+import './App.css'
+
+
+function Cart(){
+    return(
+        <div className="App">
+    <header>
+    <div className="overlay"></div>
+
+<nav>
+    <h2>Shop</h2>
+    <ul>
+        <li><a href='#'>Home</a></li>
+        <li><a href='#'>About</a></li>
+        <li><a href='#'>Cart</a></li>
+    </ul>
+</nav>
+</header>
+        </div>
+    )
+}
+
 
 
 export default Cart
