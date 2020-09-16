247b47281d07d2ce60c881859455c4cf117b118f
kendalldc <kdendtler@gmail.com>
2020-09-16 15:30:51 -0400

    insert header logo prototype

--- heir/src/Components/Header/index.js
+++ heir/src/Components/Header/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import './style.css';
-
+import Wild from './Images/Wild.png'
 const Header = props =>{
     return(
         <header className="header">
-
+<div className='wrap'>
+<div className='logo'>
+    <img src={Wild} alt="Born to be Wild">
+    </div>
+</div>
         </header>
-    )
-}
+    );
+};
 
 
 
