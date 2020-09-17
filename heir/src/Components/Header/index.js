6c7ccef5724a99d903b38c795b85356559604044
kendalldc <kdendtler@gmail.com>
2020-09-17 10:43:08 -0400

    logo edits

--- heir/src/Components/Header/index.js
+++ heir/src/Components/Header/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import './style.css';
-import Wild from './Images/Wild.png'
+// import Wild from './../../Images/Wild.png'; logo import
+
+
+
 const Header = props =>{
     return(
         <header className="header">
-<div className='wrap'>
+<div className='wrap'> 
 <div className='logo'>
-    <img src={Wild} alt="Born to be Wild">
+    {/* <img src={Wild} alt="Born to be Wild"/> */}
     </div>
 </div>
         </header>
